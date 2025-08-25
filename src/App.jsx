456010c435bec4e825c93d9f6e34453035afec3a
Mariam Ashraf <135599579+mariammhassann@users.noreply.github.com>
2025-08-25 04:34:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import ThemeToggleButton from "./components/ThemeToggleButton.jsx";
 function App() {
 
   return (
-    
-
-
     <div>
       <h1 style={{ textAlign: "center" }}>Team Dashboard</h1>
       <div style={{ display: "flex", justifyContent: "center", marginBottom:15 }}>
