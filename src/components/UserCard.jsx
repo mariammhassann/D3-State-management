707439ae853be33030596463ab37db0f77bf2718
Mariam Ashraf <135599579+mariammhassann@users.noreply.github.com>
2025-08-25 04:42:27 +0300

    Update UserCard.jsx

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,18 @@
-// src/components/UserCard.jsx
-import "./UserCard.css"
+import "./UserCard.css";
+import defaultAvatar from "../assets/images/default-avatar.jpg";
 
-import defaultAvatar from "../assets/images/default-avatar.jpg"
-
-function UserCard({ name, role, image }) {
+function UserCard({ name, role, image, cardColor }) {
   return (
-    <div className="user-card">
-      <img src={image || defaultAvatar} alt={name} className="user-image" />
+    <div className={`user-card ${cardColor}`}>
+      <img
+        src={image || defaultAvatar}
+        alt={name}
+        className="user-image"
+      />
       <h2>{name}</h2>
       <p>{role}</p>
     </div>
-  )
+  );
 }
 
-export default UserCard
+export default UserCard;
